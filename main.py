f5febeda223fa37cc219d38347590e52805f29fe
leaf48 <watavita2004@gmail.com>
2023-07-01 15:40:02 -0400

    Added config

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 from src.Twitch import Twitch
 from src.TSfile import TSfile
+import yaml
+import subprocess
+import os
+
+def yamlLoader():
+    with open("config.yaml", "r") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+        return config["streamId"]
+        
 
 if __name__ == "__main__":
-    t = Twitch().downloadM3U8List("1769579662")
+    config = yamlLoader()
+    
+    t = Twitch().downloadM3U8List("{}".format(config))
 
     ts = TSfile(t)
     ts.downloadTS()
     ts.mergeTS("./ts_files", "output.ts")
+
+    # Start convert output.ts to output.mp4
+    command = "ffmpeg -i output.ts -c:v copy -c:a copy output.mp4"
+    res = subprocess.run(command, shell=True)
+    print(res)
+    
+    # remove all .ts file
+    for filename in os.listdir("./ts_files"):
+        if filename.endswith(".ts"):
+            os.remove("./ts_files/{}".format(filename))
+    os.remove("./output.ts")
+    os.remove("./playlist.m3u8")
+
+    
